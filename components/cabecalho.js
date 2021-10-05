ddf9d088c42f0c8743b161ed71620d5e5a42d24c
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2021-10-05 09:49:29 -0300

    Tag Analytics
    Adicionando o Google Analytics ao HotSite.

--- components/cabecalho.js
+++ components/cabecalho.js
@@ -6,6 +6,8 @@ function Cabecalho (){
             <meta charSet="UTF-8"/>
             <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
             <title>CDL Cachoeiro</title>
+            <script async src="https://www.googletagmanager.com/gtag/js?id=G-4GYN48T7E6"></script>
+            <script src="scripts.js"></script>
             </head>
         </div>
     )
---
+++ components/scripts.js
@@ -0,0 +1,9 @@
+////////////////////////////
+//Tag do Google Analytics//
+//////////////////////////
+
+window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-4GYN48T7E6');

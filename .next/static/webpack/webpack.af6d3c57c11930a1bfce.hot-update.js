da294a22c1acbf990bd486d7b7d87e76ef724ce8
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2021-11-03 08:58:56 -0300

    Simple Update
    Tentativa de encaixar a tag do google ads no site.

--- .next/static/webpack/webpack.af6d3c57c11930a1bfce.hot-update.js
+++ .next/static/webpack/webpack.af6d3c57c11930a1bfce.hot-update.js
@@ -11,7 +11,7 @@ self["webpackHotUpdate_N_E"]("webpack",{},
 /******/ function(__webpack_require__) { // webpackRuntimeModules
 /******/ /* webpack/runtime/getFullHash */
 /******/ !function() {
-/******/ 	__webpack_require__.h = function() { return "ece5439c5ce67b79e9b5"; }
+/******/ 	__webpack_require__.h = function() { return "ac68273715aeff17d362"; }
 /******/ }();
 /******/ 
 /******/ }
--- components/cabecalho.js
+++ components/cabecalho.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Script from 'next/script'
 
 function Cabecalho (){
     return(
@@ -7,6 +8,10 @@ function Cabecalho (){
                 <meta charSet="UTF-8"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
                 <title>CDL Cachoeiro</title>
+                
+                <Script async src="https://www.googletagmanager.com/gtag/js?id=AW-823462958"></Script>
+                <Script async src="../lib/taggoogle.js" ></Script>
+                
             </Head>
         </>
     )

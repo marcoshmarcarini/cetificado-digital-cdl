502ed96a45c9ea3571f2d360048b88bc0a2b236b
marcoshmarcarini <marcoshmarcarini@hotmail.com>
2022-01-24 10:09:06 -0300

    Simple Update
    Adição da Tag do Facebook para o pixel.

--- pages/_documents.js
+++ pages/_documents.js
@@ -21,11 +21,31 @@ class MyDocument extends Document {
         />
         <Head />
         <body>
+        <Script
+        strategy="lazyOnLoad"
+        dangerouslySetInnerHTML={{
+            __html: `!function(f,b,e,v,n,t,s)
+                    {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+                    n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+                    if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+                    n.queue=[];t=b.createElement(e);t.async=!0;
+                    t.src=v;s=b.getElementsByTagName(e)[0];
+                    s.parentNode.insertBefore(t,s)}(window, document,'script',
+                    'https://connect.facebook.net/en_US/fbevents.js');
+                    fbq('init', '491643972326849');
+                    fbq('track', 'PageView');`,
+        }}
+
+
+        />
           <Main />
           <NextScript />
             <noscript>
                 <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NW29PBT"
                 height="0" width="0" style="display:none;visibility:hidden"></iframe>
+            </noscript>
+            <noscript>
+                <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=491643972326849&ev=PageView&noscript=1"/>
             </noscript>
         </body>
       </Html>
